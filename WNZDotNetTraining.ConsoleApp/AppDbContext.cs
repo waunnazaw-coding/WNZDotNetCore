8dcd72419547b3e9f24a01d92d55f30cd4959569
waunnazaw-coding <yeminhtet3118@gmail.com>
2024-10-12 19:52:20 +0630

    Add Sample_StudentManagemetnApp

--- WNZDotNetTraining.ConsoleApp/AppDbContext.cs
+++ WNZDotNetTraining.ConsoleApp/AppDbContext.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WNZDotNetTraining.ConsoleApp.Models;
+using WNZDotNetTraining.ConsoleApp.Models;    
 
 namespace WNZDotNetTraining.ConsoleApp
 {
