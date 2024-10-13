477cb12cb6e8145c8290ea37cda6a799b1570118
waunnazaw-coding <yeminhtet3118@gmail.com>
2024-10-13 18:45:22 +0630

    feat: asp.net core web api (crud) ado.net and dapper

---
+++ WNZDotNetTraining.RestApi/DataModels/BlogDataModels.cs
@@ -0,0 +1,11 @@
+﻿namespace WNZDotNetTraining.RestApi.DataModels
+{
+    public class BlogDataModels
+    {
+        public int BlogId { get; set; } 
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
---
+++ WNZDotNetTraining.RestApi/ViewModels/BlogViewModels.cs
@@ -0,0 +1,16 @@
+﻿namespace WNZDotNetTraining.RestApi.ViewModels
+{
+    public class BlogViewModels
+    {
+        public int Id { get; set; }
+
+        public string ? Title { get; set; }
+
+        public string  ? Author { get; set; }
+
+        public string  ? Content { get; set; }
+
+        public bool DeleteFlag { get; set; }
+    }
+}
+
