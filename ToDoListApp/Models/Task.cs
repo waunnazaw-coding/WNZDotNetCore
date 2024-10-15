86868218f51b92c874213f1abb9bc5a275fbd336
waunnazaw-coding <yeminhtet3118@gmail.com>
2024-10-15 12:52:48 +0630

    feat : Models / class Task

---
+++ ToDoListApp/Models/Task.cs
@@ -0,0 +1,24 @@
+﻿namespace ToDoListApp.Models
+{
+    public class Task
+    {
+
+        public int TaskId { get; set; }
+
+        public string TaskName { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public int ? CategoryId { get; set; }
+
+        public string PriorityLevel { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime DueDate { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime CompletedDate { get; set; }
+    }
+}
