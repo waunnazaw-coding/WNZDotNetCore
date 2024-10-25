c35748b3baba2eb53b982a88b2a6c169e1b92bea
waunnazaw-coding <yeminhtet3118@gmail.com>
2024-10-25 23:54:25 +0630

    feat : add AppDbContext

---
+++ ToDoListApp/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListApp.Models;
+
+namespace ToDoListApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Data Source = . ; Initial Catalog =  WNZDotNet ; User ID = sa ; password = waunnazaw ; TrustServerCetificate = true";
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+
+    }
+}
